368e501bcd90a3161640856c821439a878c9c807
Victor <psygreg@pm.me>
2025-08-14 14:13:09 -0300

    backport fix for Arch optimized defaults

--- appimage/subscripts/pdefaults.sh
+++ appimage/subscripts/pdefaults.sh
@@ -7,13 +7,6 @@ sysag_run () {
         cachyos_sysd_lib
     fi
     sboost_lib
-    # set safe minimum ram to use preload
-    local total_kb=$(grep MemTotal /proc/meminfo | awk '{ print $2 }')
-    local total_gb=$(( total_kb / 1024 / 1024 ))
-    _cram=$(( total_gb ))
-    if (( _cram < 16 )); then
-        preload_lib
-    fi
     dsplitm_lib
     flatpak_in_lib
     if command -v flatpak &> /dev/null; then
@@ -46,15 +39,9 @@ optimizer () {
             sysag_run
             zeninf "$msg036"
         elif [[ "$ID_LIKE" =~ (rhel|fedora) ]] || [ "$ID" == "fedora" ]; then
-            if zenity --question --text "$msg150" --width 360 --height 300; then
-                fedora_cachyos_menu_lib
-            fi
             # run system-agnostic optimizations
             sysag_run
         elif [[ "$ID" =~ ^(arch)$ ]] || [[ "$ID_LIKE" == *arch* ]] || [[ "$ID_LIKE" == *archlinux* ]]; then
-            if zenity --question --text "$msg150" --width 360 --height 300; then
-                cachyos_arch_lib
-            fi
             # run system-agnostic optimizations
             sysag_run
             zeninf "$msg036"
